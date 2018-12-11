4ce4102e322bbd5e325774658ea1491e5e8121a2
Sebastian Lequime <30280751+slequime@users.noreply.github.com>
2018-12-11 16:47:14 +0100

    Update nosoiSim.R

--- R/nosoiSim.R
+++ R/nosoiSim.R
@@ -1,5 +1,5 @@
 nosoiSim <- function(length=NA, max.infected=NA,type="single",geo="none",parallel=FALSE,...){
-library(data.table)
+  library(data.table)
   #Sanity checks -------------
   if (is.na(length) | length <= 1) message("You must specify a length (in time units) for your simulation.")
   if (is.na(max.infected) | max.infected <= 1) message("You must specify a maximum number of infected hosts.")
@@ -9,7 +9,6 @@ library(data.table)
 
   #Loading correct script ------------------
   if(type=="single" & geo=="none" & parallel==FALSE) {
-    source("./single-none-nonparallel.R")
     single_none_parallel(length=length,max.infected=max.infected,...)
 }
   #To be implemented
