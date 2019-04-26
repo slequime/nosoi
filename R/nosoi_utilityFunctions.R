c7e24278f75c0fa4b0c69dcfcdb1f695bd990891
Sebastian Lequime <30280751+slequime@users.noreply.github.com>
2019-04-26 17:15:22 +0200

    Continue the work on the tutorials

--- R/nosoi_utilityFunctions.R
+++ R/nosoi_utilityFunctions.R
@@ -3,11 +3,11 @@
 #' @description
 #' Echos the state of the simulation at any given time step provided by the user.
 #'
-#' @param Host.count
-#' @param pres.time
-#' @param print.step
-#' @param length.sim
-#' @param max.infected
+#' @param Host.count number of infected hosts
+#' @param pres.time current time of the simulation
+#' @param print.step progress.bar is TRUE, step with which the progress message will be printed.
+#' @param length.sim the length (in unit of time) over which the simulation should be run.
+#' @param max.infected the maximum number of hosts that can be infected in the simulation.
 #'
 #'
 #' @keywords internal
@@ -22,8 +22,8 @@ progressMessage <- function(Host.count, pres.time, print.step, length.sim, max.i
 #' @description
 #' Message that ends the simulation
 #'
-#' @param Host.count
-#' @param pres.time
+#' @param Host.count number of infected hosts
+#' @param pres.time current time of the simulation
 #'
 #'
 #' @keywords internal
@@ -38,10 +38,10 @@ endMessage <- function(Host.count, pres.time) {
 #' @description
 #' Wraps up the nosoi output
 #'
-#' @param Host.count
-#' @param pres.time
-#' @param table.hosts
-#' @param state.archive
+#' @param Host.count number of infected hosts
+#' @param pres.time current time of the simulation
+#' @param table.hosts data.table of hosts
+#' @param state.archive data.table of hosts movement
 #'
 #'
 #' @keywords internal
